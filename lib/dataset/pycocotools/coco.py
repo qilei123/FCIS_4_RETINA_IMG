998a2ad5d9c0551bdf9ccef5a6ee23aeaad5bd31
qilei123 <qileimail123@gmail.com>
2018-11-19 13:21:33 -0500

    pre train

--- lib/dataset/pycocotools/coco.py
+++ lib/dataset/pycocotools/coco.py
@@ -76,6 +76,7 @@ class COCO:
         if not annotation_file == None:
             print 'loading annotations into memory...'
             tic = time.time()
+            print annotation_file
             dataset = json.load(open(annotation_file, 'r'))
             print 'Done (t=%0.2fs)'%(time.time()- tic)
             self.dataset = dataset
