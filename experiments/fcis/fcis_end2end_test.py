fd29f8ddd483ec6812fd2b2a1d06ada2ee2641ef
qilei123 <qileimail123@gmail.com>
2018-12-04 11:47:44 -0500

    fdssa

--- experiments/fcis/fcis_end2end_test.py
+++ experiments/fcis/fcis_end2end_test.py
@@ -5,8 +5,12 @@ os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
 os.environ['MXNET_ENABLE_GPU_P2P'] = '0'
 this_dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(this_dir, '..', '..', 'fcis'))
-
+from config.config import config
 import test
 
 if __name__ == "__main__":
-    test.main()
+    test_epoch_list = [8,9,10,11,12,13,14,15,16,17]
+    for test_epoch in test_epoch_list:
+        print 'test_epoch:'+test_epoch
+        config.TEST.test_epoch = test_epoch
+        test.main()
