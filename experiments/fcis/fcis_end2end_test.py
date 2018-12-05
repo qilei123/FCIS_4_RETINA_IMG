efb46999348523b3ef3f6dd244f22e7b4b78ead2
qilei123 <qileimail123@gmail.com>
2018-12-05 12:45:10 -0500

    fasd

--- experiments/fcis/fcis_end2end_test.py
+++ experiments/fcis/fcis_end2end_test.py
@@ -9,7 +9,7 @@ from config.config import config
 import test
 
 if __name__ == "__main__":
-    test_epoch_list = [6,7,8,9,10,11,12,13]
+    test_epoch_list = [43]
     for test_epoch in test_epoch_list:
         print 'test_epoch:'+str(test_epoch)
         config.TEST.test_epoch = test_epoch
