c19fbe64ffff4325e10ca7e6f7b68f3b9518085a
qilei123 <qileimail123@gmail.com>
2018-12-21 09:01:33 -0500

    fdad

--- train_DR_dcn_v1_no_ohem.sh
+++ train_DR_dcn_v1_no_ohem.sh
@@ -1,1 +1,1 @@
-python experiments/fcis/fcis_end2end_train_test.py --cfg experiments/fcis/cfgs/resnet_v1_101_DR_fcis_dcn_v1_end2end_ohem.yaml
+python experiments/fcis/fcis_end2end_train_test.py --cfg experiments/fcis/cfgs/resnet_v1_101_DR_fcis_dcn_v1_end2end.yaml
