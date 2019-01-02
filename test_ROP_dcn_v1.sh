03d290fe2346c84e9275d4995404987c9f18c0a4
qilei123 <qileimail123@gmail.com>
2019-01-02 15:21:25 -0500

    fas

--- test_ROP_dcn_v1.sh
+++ test_ROP_dcn_v1.sh
@@ -1,1 +1,1 @@
-python experiments/fcis/fcis_end2end_test.py --cfg experiments/fcis/cfgs/resnet_v1_101_ROP_fcis_dcn_v1_end2end_ohem.yaml --epoch 8
+python experiments/fcis/fcis_end2end_test.py --cfg experiments/fcis/cfgs/resnet_v1_101_ROP_fcis_dcn_v1_end2end_ohem.yaml --test_epoch 8
