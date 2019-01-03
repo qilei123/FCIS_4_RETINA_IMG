b60141e1bd63336b1c5219f865b12981c8e0c88f
qilei123 <qileimail123@gmail.com>
2019-01-03 12:42:51 -0500

    fdad

--- experiments/fcis/fcis_end2end_test.py
+++ experiments/fcis/fcis_end2end_test.py
@@ -2,7 +2,7 @@ import os
 import sys
 os.environ['PYTHONUNBUFFERED'] = '1'
 os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
-os.environ['MXNET_ENABLE_GPU_P2P'] = '2'
+os.environ['MXNET_ENABLE_GPU_P2P'] = '0'
 this_dir = os.path.dirname(__file__)
 sys.path.insert(0, os.path.join(this_dir, '..', '..', 'fcis'))
 from config.config import config
